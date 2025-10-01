97aa0d7ca2a28aefb30f2f189f2fda5f8215f292
uqayiyufog07-eng <uqayiyufog07@gmail.com>
2025-10-01 09:41:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,7 +51,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/uqayiyufog07-eng',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
